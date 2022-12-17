b5ca0e3d3e57253367335fc424bc61de816f9d02
h.saito <ruru9583@gmail.com>
2022-12-17 17:53:09 +0900

    テキストをDBに保存できるようになった

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Memo;
 
 class HomeController extends Controller
 {
@@ -24,5 +25,27 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+
+    public function create()
+    {
+        $user =\Auth::user();
+        return view('create', compact('user'));
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        // dd($data);
+        // POSTされたデータをDB（memosテーブル）に挿入
+        // MEMOモデルにDBへ保存する命令を出す
+
+        // タグIDをmemosテーブルに入れてあげる
+        $memo_id = Memo::insertGetId
+            (['content' => $data['content'], 'user_id' => $data['user_id'], 'status' => 1 
+        ]);
+        
+        // リダイレクト処理
+        return redirect()->route('home');
     }
 }
