ca4850f4bf64b66908b65c3883a189daef34b559
h.saito <ruru9583@gmail.com>
2022-12-18 22:47:57 +0900

    テキストの編集ができるようになった　 $memosも解決済み

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,14 +26,17 @@ class HomeController extends Controller
     {
         $user =\Auth::user();
         //入力したテキストの取得
-        $memo = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
-        return view('home', compact('user', 'memo'));
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        return view('home', compact('user', 'memos'));
     }
 
     public function create()
     {
         $user =\Auth::user();
-        return view('create', compact('user'));
+        //入力したテキストの取得
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        return view('create', compact('user', 'memos'));
+
     }
 
     public function store(Request $request)
@@ -51,4 +54,26 @@ class HomeController extends Controller
         // リダイレクト処理
         return redirect()->route('home');
     }
+
+    public function edit($id){
+        // 該当するIDのメモをデータベースから取得
+        $user = \Auth::user();
+        $memo = Memo::where('status', 1)->where('id', $id)->where('user_id', $user['id'])
+          ->first();
+        //  dd($memo);
+
+        $memos = Memo::where('user_id', $user['id'])->where('status', 1)->orderBy('updated_at', 'DESC')->get();
+        //取得したメモをViewに渡す
+        return view('edit',compact('memo', 'user', 'memos'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $inputs = $request->all();
+        // dd($inputs);
+        Memo::where('id', $id)->update(['content' => $inputs['content'] ]);
+        return redirect()->route('home');
+    }
+
+
 }
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -3,16 +3,16 @@
 @section('content')
 <div class="row justify-content-center ml-0 mr-0 h-100">
     <div class="card w-100">
-        <div class="card-header">新規メモ作成</div>
+        <div class="card-header">メモ編集</div>
         <div class="card-body">
-            <form method='POST' action="/store">
+            <form method='POST' action="{{ route('update', ['id' => $memo['id'] ] ) }}">
                 @csrf
                 <input type='hidden' name='user_id' value="{{ $user['id'] }}">
                 <div class="form-group">
-                     <textarea name='content' class="form-control"rows="10"></textarea>
+                     <textarea name='content' class="form-control"rows="10">{{ $memo['content'] }}</textarea>
                 </div>
 <!--タグを表示させる-->
-                <button type='submit' class="btn btn-primary btn-lg">保存</button>
+                <button type='submit' class="btn btn-primary btn-lg">更新</button>
             </form>
         </div>
     </div>
